5b7fb6d1f6ee6df3923ef6af3f2a4d8a3860b5fd
Norbert <norbert@softgenius.com>
2023-03-06 12:00:15 +0200

    Test nuxt-img dynamic url

--- app.vue
+++ app.vue
@@ -112,7 +112,7 @@
               class="pic swiper-container-1"
               @click="() => showImg(index)"
             >
-              <img :src="`/${src}`" alt="image"/>
+              <nuxt-img :src="src" alt="image"/>
             </swiper-slide>
           </swiper>
           <div class="arrows-container">
@@ -217,16 +217,16 @@ export default defineComponent({
     const now = new Date().getTime();
     const newYear = new Date("April 5, 2023 15:37:25").getTime();
     const imgs = [
-      "assets/poze/a0.jpg",
-      "assets/poze/a1.jpg",
-      "assets/poze/a2.jpg",
-      "assets/poze/a3.jpg",
-      "assets/poze/a4.jpg",
-      "assets/poze/a5.jpg",
-      "assets/poze/a6.jpg",
-      "assets/poze/a7.jpg",
-      "assets/poze/a8.jpg",
-      "assets/poze/a9.jpg",
+      "/assets/poze/a0.jpg",
+      "/assets/poze/a1.jpg",
+      "/assets/poze/a2.jpg",
+      "/assets/poze/a3.jpg",
+      "/assets/poze/a4.jpg",
+      "/assets/poze/a5.jpg",
+      "/assets/poze/a6.jpg",
+      "/assets/poze/a7.jpg",
+      "/assets/poze/a8.jpg",
+      "/assets/poze/a9.jpg",
     ];
     const showImg = (index) => {
       indexRef.value = index;
